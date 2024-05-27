5a85e961f43a14722ef59905127a767c58f2e611
Obusker <mr.kudryachenko@gmail.com>
2024-05-27 13:26:39 +0300

    commit

--- app/main.py
+++ app/main.py
@@ -9,25 +9,13 @@ def format_linter_error(error: dict) -> dict:
 
 def format_single_linter_file(file_path: str, errors: list) -> dict:
     return {
-        "errors": [{"line": second_error["line_number"],
-                    "column": second_error["column_number"],
-                    "message": second_error["text"],
-                    "name": second_error["code"],
-                    "source": "flake8"}
-                   for second_error in errors],
+        "errors": [format_linter_error(error) for error in errors],
         "path": file_path,
-        "status": "passed" if len(errors) is None else "failed"}
+        "status": "passed" if len(errors) < 1 else "failed"}
 
 
 def format_linter_report(linter_report: dict) -> list:
     return [
-        {"errors": [{"line": third_error["line_number"],
-                     "column": third_error["column_number"],
-                     "message": third_error["text"],
-                     "name": third_error["code"],
-                     "source": "flake8"}
-                    for third_error in errors],
-         "path": keys,
-         "status": "passed" if len(errors) < 1 else "failed"}
+        format_single_linter_file(keys, errors)
         for keys, errors in linter_report.items()
     ]
